6a4b8bd66bf46bc1de090b7a0dee2a5b55c5043e
Samuel Miller <1098623@cslaval.qc.ca>
2013-06-16 14:38:05 -0400

    Changed readme to fit new features:
    -You can now put main.py and the other files it needs in any folder you
    want! You must only write the path of the folder in folder.txt, as said
    in the README.
    
    -As the "title" says, the README was changed for people to know that
    they need to write the path in the folder.txt file.
    
    -Obviously, folder.txt file added.

--- README.txt
+++ README.txt
@@ -8,6 +8,9 @@ Made by me (Py_hacker). Only I can do what I want with that. In other words: cop
 To play this RPG, you WILL need Pygame, Python (Obviously) and everything Pygame needs to run. If not sure of what to download, download what you need depending of 
 what you are using (Linux, Windows, etc.) here: www.manning.com/sande
 
+Also, don't forget to write the FULL path of the folder where the main.py file is in the folder.txt file! Also, don't 
+separate the main.py file and the other files and folders! Don't rename the folders in Other_code and Sprites!
+
 Wanna base your work out of that or modify it? Do it, but please state the original author of the original game (me).
 
 »»End of README.txt
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import pygame, sys
-
 print "Loading..."
 
-background = pygame.image.load("C:/Users\Miller/Desktop/Python RPG repo/A-Python-RPG/Sprites/title/background.png")
-title = pygame.image.load("C:/Users\Miller/Desktop/Python RPG repo/A-Python-RPG/Sprites/title/title.png")
+import pygame, sys
+
+folder = open('folder.txt', 'r')
+folderinfo = folder.readline()
+
+background = pygame.image.load(folderinfo + "/Sprites/title/background.png")
+title = pygame.image.load(folderinfo + "/Sprites/title/title.png")
+
 print "Loaded ALL the things!"
 
 screen = pygame.display.set_mode([744, 744])
@@ -11,6 +15,7 @@ screen = pygame.display.set_mode([744, 744])
 screen.blit(background, [0, 0])
 screen.blit(title, [135, 47])
 pygame.display.flip()
+folder.close
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
