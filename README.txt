dfa9a1d49ca375457263c50ec2c5834c0437e395
Samuel Miller <1098623@cslaval.qc.ca>
2013-07-07 20:04:03 -0400

    README update AGAIN
    This is a update to update the README, but I'm commiting other files so
    that you can see how it's going.

--- README.txt
+++ README.txt
@@ -5,12 +5,14 @@ THE DIAMOND ROCKS
 
 Made by me (Py_hacker). Only I can do what I want with that. In other words: copyright Py_hacker
 
-To play this RPG, you WILL need Pygame, Python (Obviously) and everything Pygame needs to run. If not sure of what to download, download what you need depending of 
-what you are using (Linux, Windows, etc.) here: www.manning.com/sande
+To play this RPG, you WILL need Pygame, Python (Obviously) and everything Pygame needs to run. If not sure of what to 
+download, download what you need depending of what you are using (Linux, Windows, etc.) here: www.manning.com/sande
+But I don't know if THE DIAMOND ROCKS works with Linux/Mac.
 
-Also, don't forget to write the FULL path of the folder where the main.py file is in the folder.txt file! Also, don't 
+Also, don't forget to write the FULL path of the folder where the main.py file is in the folder.txt file! Don't 
 separate the main.py file and the other files and folders! Don't rename the folders in Other_code and Sprites!
 
-Wanna base your work out of that or modify it? Do it, but please state the original author of the original game (me).
+For better gaming experience, use a PC with a computer screen bigger than 744 x 744. And use Stani's Python Editor, which
+is in the "pack" downloaded at www.manning.com/sande 
 
 »»End of README.txt
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ title = pygame.image.load(folderinfo + "/Sprites/title/title.png")
 play_button = pygame.image.load(folderinfo + "/Sprites/title/interface/play_button.png")
 options_button = pygame.image.load(folderinfo + "/Sprites/title/interface/options_button.png")
 button_overlay_1 = pygame.image.load(folderinfo + "/Sprites/title/interface/overlay_1.png")
+save1 = pygame.image.load(folderinfo + "/Sprites/title/interface/saves/Save_1.png")
+save2 = pygame.image.load(folderinfo + "/Sprites/title/interface/saves/Save_2.png")
+save3 = pygame.image.load(folderinfo + "/Sprites/title/interface/saves/Save_3.png")
 print "Loaded ALL the things!"
 
 screen = pygame.display.set_mode([744, 744])
@@ -23,34 +26,91 @@ overlay_pos = 0
 pygame.display.flip()
 folder.close
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                if overlay_pos == 0:
-                    screen.blit(background, [0, 0])
-                    screen.blit(title, [135, 47])
-                    screen.blit(play_button, [253, 300])
-                    screen.blit(options_button, [253, 441])
-                    screen.blit(button_overlay_1, [243, 431])                    
-                    pygame.display.flip()
-                    overlay_pos = 1
-                else:
-                    pass
-            elif event.key == pygame.K_UP:
-                if overlay_pos == 1:
-                    screen.blit(background, [0, 0])
-                    screen.blit(title, [135, 47])
-                    screen.blit(play_button, [253, 300])
-                    screen.blit(options_button, [253, 441])
-                    screen.blit(button_overlay_1, [243, 290])
-                    pygame.display.flip()
-                    overlay_pos = 0
-                else:
-                    pass
-            elif event.key == pygame.K_RETURN:
-                if overlay_pos == 0:
-                    print overlay_pos
-                elif overlay_pos == 1:
-                    print overlay_pos
+    screen.blit(background, [0, 0])
+    screen.blit(title, [135, 47])
+    screen.blit(play_button, [253, 300])
+    screen.blit(options_button, [253, 441])
+    screen.blit(button_overlay_1, [243, 290])
+    overlay_pos = 0
+    pygame.display.flip()
+    stay1 = True
+    while stay1 == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if overlay_pos == 0:
+                        screen.blit(background, [0, 0])
+                        screen.blit(title, [135, 47])
+                        screen.blit(play_button, [253, 300])
+                        screen.blit(options_button, [253, 441])
+                        screen.blit(button_overlay_1, [243, 431])
+                        pygame.display.flip()
+                        overlay_pos = 1
+                    else:
+                        pass
+                elif event.key == pygame.K_UP:
+                    if overlay_pos == 1:
+                        screen.blit(background, [0, 0])
+                        screen.blit(title, [135, 47])
+                        screen.blit(play_button, [253, 300])
+                        screen.blit(options_button, [253, 441])
+                        screen.blit(button_overlay_1, [243, 290])
+                        pygame.display.flip()
+                        overlay_pos = 0
+                    else:
+                        pass
+                elif event.key == pygame.K_RETURN:
+                    if overlay_pos == 0:
+                        screen.blit(background, [0, 0])
+                        screen.blit(save1, [93, 30])
+                        screen.blit(save2, [93, 278])
+                        screen.blit(save3, [93, 526])
+                        screen.blit(button_overlay_1,[83, 20])
+                        overlay_pos2 = 0 
+                        pygame.display.flip()
+                        stay2 = True
+                        while stay2 == True:
+                            for event in pygame.event.get():
+                                if event.type == pygame.KEYDOWN:
+                                    if event.key == pygame.K_DOWN:
+                                        if overlay_pos2 == 0:
+                                            screen.blit(background, [0, 0])
+                                            screen.blit(save1, [93, 30])
+                                            screen.blit(save2, [93, 278])
+                                            screen.blit(save3, [93, 526])
+                                            screen.blit(button_overlay_1,[83, 268])
+                                            overlay_pos2 = 1
+                                            pygame.display.flip()
+                                        elif overlay_pos2 == 1:
+                                            screen.blit(background, [0, 0])
+                                            screen.blit(save1, [93, 30])
+                                            screen.blit(save2, [93, 278])
+                                            screen.blit(save3, [93, 526])
+                                            screen.blit(button_overlay_1,[83, 516])
+                                            overlay_pos2 = 2
+                                            pygame.display.flip()
+                                        else:
+                                            pass
+                                    elif event.key == pygame.K_UP:
+                                        if overlay_pos2 == 1:
+                                            screen.blit(background, [0, 0])
+                                            screen.blit(save1, [93, 30])
+                                            screen.blit(save2, [93, 278])
+                                            screen.blit(save3, [93, 526])
+                                            screen.blit(button_overlay_1,[83, 20])
+                                            overlay_pos2 = 0 
+                                            pygame.display.flip()
+                                        elif overlay_pos2 == 2:
+                                            screen.blit(background, [0, 0])
+                                            screen.blit(save1, [93, 30])
+                                            screen.blit(save2, [93, 278])
+                                            screen.blit(save3, [93, 526])
+                                            screen.blit(button_overlay_1,[83, 268])
+                                            overlay_pos2 = 1
+                                            pygame.display.flip()
+                                        else:
+                                            pass
+                    elif overlay_pos == 1:
+                        print "Options aren't implemented yet. I do confirm there will be a language option. That won't be easy to do..."
